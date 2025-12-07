1c8f4bfacdad97352e8d109d33d633ede2b471fa
vipul0072 <rawatvipul14@gmail.com>
2025-12-07 20:47:48 +0530

    Clean history - removed commit with secret

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,7 @@
 import '../styles/About.css';
-import img from '../assets/img1.jpg';
+import img from '../assets/vipul.jpg';
+import img2 from '../assets/img1.jpg';
+import img3 from '../assets/img2.jpg';
 
 function About() {
     return (
@@ -9,29 +11,33 @@ function About() {
                     <div className="image">
                         <img src={img} alt="Mr. Vipul Rawat" />
                         <h3>Mr. Vipul Rawat</h3>
+                        <button className='story' href="">Read full story</button>
                     </div>
                     <div className="message">
-                        <h2>Director's Message</h2>
+                        <h2>Founder & CEO</h2>
+                        <p>As Director of Summit Seekers, my passion for the mountains began with childhood treks through Uttarakhand's rugged trails, fueling a lifelong commitment to sharing these transformative experiences. Our team crafts personalized adventures—from beginner hikes to challenging expeditions—that prioritize safety, sustainability, and unforgettable moments in nature's embrace.</p>
+                        
+                    </div>
+                </div>
+                <div className="director-card">
+                    <div className="image">
+                        <img src={img2} alt="Mr. Vipul Rawat" />
+                        <h3>Co-Founder</h3>
+                        <button className='story' href="">Read full story</button>
+                    </div>
+                    <div className="message">
+                        <h2>Co-Founder & Partners</h2>
                         <p>As Director of Summit Seekers, my passion for the mountains began with childhood treks through Uttarakhand's rugged trails, fueling a lifelong commitment to sharing these transformative experiences. Our team crafts personalized adventures—from beginner hikes to challenging expeditions—that prioritize safety, sustainability, and unforgettable moments in nature's embrace.</p>
                     </div>
                 </div>
                 <div className="director-card">
                     <div className="image">
-                        <img src={img} alt="Mr. Vipul Rawat" />
-                        <h3>Mr. Vipul Rawat</h3>
+                        <img src={img3} alt="Mr. Vipul Rawat" />
+                        <h3>Marketing Head</h3>
+                        <button className='story' href="">Read full story</button>
                     </div>
                     <div className="message">
-                        <h2>Director's Message</h2>
-                        <p>As Director of Summit Seekers, my passion for the mountains began with childhood treks through Uttarakhand's rugged trails, fueling a lifelong commitment to sharing these transformative experiences. Our team crafts personalized adventures—from beginner hikes to challenging expeditions—that prioritize safety, sustainability, and unforgettable moments in nature's embrace.</p>
-                    </div>
-                </div>
-                <div className="director-card">
-                    <div className="image">
-                        <img src={img} alt="Mr. Vipul Rawat" />
-                        <h3>Mr. Vipul Rawat</h3>
-                    </div>
-                    <div className="message">
-                        <h2>Director's Message</h2>
+                        <h2>Co-Founder & Marketing Head</h2>
                         <p>As Director of Summit Seekers, my passion for the mountains began with childhood treks through Uttarakhand's rugged trails, fueling a lifelong commitment to sharing these transformative experiences. Our team crafts personalized adventures—from beginner hikes to challenging expeditions—that prioritize safety, sustainability, and unforgettable moments in nature's embrace.</p>
                     </div>
                 </div>

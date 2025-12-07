c098c98f8235cf1092cfde4a534df343d6a6e786
vipul0072 <rawatvipul14@gmail.com>
2025-12-07 20:02:14 +0530

    Clean history - removed commit with secret

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
             <div className="home-background">
                     <div className="home-content">
                         <h1 className='home-content-h1'>Adventure and Treks</h1>
-                        <p className='home-content-para'>Adventrue give a way to live your life and gather some memories.</p>
+                        <p className='home-content-para'>Adventure give a way to live your life and gather some memories.</p>
                         <button className='home-content-button'>Book Your Next Trek</button>
                     </div>
             </div>
